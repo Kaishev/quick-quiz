c21f3a1699400817e4dd1895493eb74c56683e4c
Kaishev <wcaishev@gmail.com>
2023-10-10 17:34:01 +0700

    mini quiz about memes

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const questions = [
   {
-    question: "Видишь там на горе...",
-    answers: ["кресвышается вост", "возвышается крест", "показал мужик топор"],
+    question: "Смотря какой fabriс...",
+    answers: ["производит шоколад", "смотря сколько details", "ААА!?", "очень afforible"],
     correct: 2,
   },
 
   {
-    question: "Where is 'u'?",
-    answers: ["u", "e", "y"],
+    question: "Хорошо что сундуки...",
+    answers: ["остались наверху", "открыты", "хранят тайны", "не взорвались"],
     correct: 1,
   },
 
@@ -20,6 +20,82 @@ const questions = [
       "ты кто такой чтоб это сделать",
     ],
     correct: 3,
+  },
+  
+  {
+    question: "Я монтирую, монтирую, а он...",
+    answers: [
+      "козёл ебаный",
+      "блять",
+      "взорвался нахуй",
+      "не монтируется",
+    ],
+    correct: 3,
+  },
+
+  {
+    question: "Так, а почему едет он? А?...",
+    answers: [
+      "а может быть ты",
+      "А?",
+      "а не ты",
+      "а не я",
+    ],
+    correct: 4,
+  },
+
+  {
+    question: "похо торо моронго и...",
+    answers: [
+      "пеперонни",
+      "АННИГИЛЯТОРНАЯ ПУШКА!",
+      "санчо панса",
+      "огнором орот охоп",
+    ],
+    correct: 2,
+  },
+  
+  {
+    question: "у нас тут два мальчика катаются, не один...",
+    answers: [
+      "в поле",
+      "да киселёчек?",
+      "и не два",
+      "не умеет",
+    ],
+    correct: 2,
+  },
+    
+  {
+    question: "Да ты хоть знаешь кто я? Я вор...",
+    answers: [
+      "в законе",
+      "и вором останусь",
+      "кхъа",
+      "...онин",
+    ],
+    correct: 3,
+  },   
+
+  {
+    question: "Начальник! Стоим...",
+    answers: [
+      "уже долго",
+      "или не стоим?",
+      "до конца",
+      "...ость минимальна",
+    ],
+    correct: 2,
+  },
+  {
+    question: "Доллар, доллар, доллар - это ...",
+    answers: [
+      "всё-таки деньги",
+      "доллар",
+      "а как иначе",
+      "грррязная зелёная бумажка",
+    ],
+    correct: 4,
   },
 ];
 
@@ -34,6 +110,7 @@ let score = 0; // кол-во правильных ответов
 
 clearPage();
 showQuestion();
+submitBtn.onclick = checkAnswer;
 
 function clearPage() {
   headerContainer.innerHTML = "";
@@ -41,28 +118,112 @@ function clearPage() {
 }
 
 function showQuestion() {
-  // показать вопрос
-  const headerTemplate = `
-    <h2 class="title">%title%</h2>
-    `;
-  const title = headerTemplate.replace(
-    "%title%",
-    questions[questionIndex]["question"]
-  );
 
-  headerContainer.innerHTML = title;
+  // показать вопрос
+    const headerTemplate = `<h2 class="title">%questionText%</h2>`;
+
+    const questionHTML = headerTemplate.replace("%questionText%", questions[questionIndex]["question"]);
+
+    headerContainer.innerHTML = questionHTML;
+
 
   // показать ответы
-  questions[questionIndex]["answers"].forEach((answerText) => {
-    const answersTemplate = `
-      <li>
-          <label>
-            <input type="radio" class="answer" name="answer" />
-            <span>%answer%</span>
-          </label>
-      </li>
+    for ([answerNumber,answerText] of questions[questionIndex]['answers'].entries()) {
+
+        const answersTemplate = `
+            <li>
+                <label>
+                    <input value="%number%" type="radio" class="answer" name="answer" />
+                    <span>%answer%</span>
+                </label>
+            </li>
+        `;
+
+        const answerHTML = answersTemplate
+                        .replace("%answer%", answerText)
+                        .replace('%number%', answerNumber+1);
+
+        answersListContainer.innerHTML += answerHTML;
+        answerNumber++;
+        console.log(answerNumber, answerText)
+    };
+}
+
+function checkAnswer() {
+    console.log('checkAnswer started!');
+
+  // Находим выбранную радиокнопку
+    const checkedRadio = answersListContainer.querySelector('input[type="radio"]:checked');
+
+  // Если ответ не выбран - выходим из функции
+    if (!checkedRadio){
+        submitBtn.blur();
+        return
+    };
+
+  // Узнаём номер ответа пользователя
+    const userAnswer = parseInt(checkedRadio.value);
+
+    // Если ответ верен - увеличиваем счёт
+    if (userAnswer === questions[questionIndex]['correct']) {
+        score++;
+    }
+
+    console.log('score = ', score);
+
+    if (questionIndex !== questions.length -1) {
+
+        questionIndex++;
+        clearPage();
+        showQuestion();
+        return;
+
+    } else {
+
+        clearPage();
+        showResults();
+
+    }
+}
+
+function showResults() {
+    console.log('showResults started!', score);
+
+    const resultTemplate = `
+        <h2 class="title">%title%</h2>
+        <h3 class="summary">%message%</h3>
+        <p class="results">%results%</p>
     `;
-    const answerHTML = answersTemplate.replace("%answer%", answerText);
-    answersListContainer.innerHTML += answerHTML;
-  });
+
+    let resultsTitle, message;
+
+    // Варианты заголовков и текста
+    if (score === questions.length) {
+        resultsTitle = 'Это успех! Да Вы эксперт по широко известным в узких кругах мемам!🎉';
+        message = 'И дали максимальное количество правильных ответов!';
+    } else if ((score * 100)/questions.length>=50) {
+        resultsTitle = 'Довольно таки неплохо... 🤔';
+        message = 'Вы дали более половины правильных ответов';
+    } else if (score === 0) {
+        resultsTitle = 'Ого! Поздравляем! 😎';
+        message = 'Вы набрали отрицательный максимум правильных ответов!'
+    } else {
+        resultsTitle = 'Стоит постараться... 🥴';
+        message = 'Пока у Вас меньше половины правильных ответов'
+    }
+    // Результат
+    let results = `${score} из ${questions.length}`;
+
+    // Финальный ответ, подставляем данные в шаблон
+    const finalMessage = resultTemplate
+                        .replace('%title%',resultsTitle)
+                        .replace('%message%',message)
+                        .replace('%results%',results);
+        
+    headerContainer.innerHTML = finalMessage;
+
+    // Меняем кнопку на Играть снова
+    submitBtn.blur();
+    submitBtn.innerText = 'Начать заново'
+    submitBtn.onclick = () => history.go();
 }
